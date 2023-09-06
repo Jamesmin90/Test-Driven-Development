a73d9cf9ad867ad186a7fc2388d18eeca9ff59ce
James Min <54721700+Jamesmin90@users.noreply.github.com>
2023-09-06 12:29:50 +0200

    Add files via upload

---
+++ tests/test_account.py
@@ -0,0 +1,42 @@
+"""
+Test Cases TestAccountModel
+"""
+import json
+from unittest import TestCase
+from models import db
+from models.account import Account
+
+ACCOUNT_DATA = {}
+
+class TestAccountModel(TestCase):
+    """Test Account Model"""
+
+    @classmethod
+    def setUpClass(cls):
+        """ Connect and load data needed by tests """
+        db.create_all()
+        global ACCOUNT_DATA
+        with open('test/fxitures/account_data.json') as json_data:
+            ACCOUNT_DATA = json.load(json_data)
+
+    @classmethod
+    def tearDownClass(cls):
+        """Disconnect from database"""
+        db.session.close()
+
+    def setUp(self):
+        """Truncate the tables"""
+        db.session.query(Account).delete()
+
+
+    def tearDown(self):
+        """Remove the session"""
+
+    ######################################################################
+    #  T E S T   C A S E S
+    ######################################################################
+    def test_create_account(self):
+        """Test creating an Account"""
+        account = Account(**ACCOUNT_DATA[0])
+        account.create()
+        self.assertEqual(len(Account.all()), 1)
